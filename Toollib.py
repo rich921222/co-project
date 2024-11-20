0f817720af8ce27edc3ad9a7395bda4b441ed163
Rich <rich921222@gmail.com>
2024-11-20 19:34:00 +0800

    Embeding 放入Toollib.py

--- Toollib.py
+++ Toollib.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from xxhash import xxh32
 import B2D
-from skimage import io
+from skimage import io,transform
 
 def APPM_RT256():
     RT = []
@@ -251,3 +251,26 @@ def Authorize(Graph, Name=None):
     if(Name != None): 
         with open("processing_data/"+Name+".txt","a") as file:
             file.write(f"PSNR: {accuracy}\n")    
+
+def embeding(image,n):
+    def noise(I,Noise):
+        n_r,n_c = Noise.shape[0],Noise.shape[1]
+
+        for i in range(n_r):
+            for j in range(n_c):
+                if(Noise[i, j,3]==0):
+                    continue
+                for k in range(3):
+                    I[i+255,j+255,k] = Noise[i,j,k]
+
+        return I
+    path = "processing_image/"+image+".png"
+    I=io.imread(path)
+    path2 = "noise/"+n+".png"
+    I2=io.imread(path2)
+    if n == 'grass':
+        I2 = transform.resize(I2, (I.shape[0]/2, I.shape[1]/2, 4))
+    e = noise(I,I2)
+
+    io.imshow(e)
+    io.imsave('embeding_noise/'+image+'.png',e) 
