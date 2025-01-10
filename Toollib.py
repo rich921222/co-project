5bbb115ca8a64a6f1d3063018f71f2549e5281e6
Rich <rich921222@gmail.com>
2025-01-10 17:09:19 +0800

    2025/01/10

--- Toollib.py
+++ Toollib.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from xxhash import xxh32
 import B2D
 from skimage import io,transform
+import random
 
 def APPM_RT256():
     RT = []
@@ -266,12 +267,14 @@ def embeding(image,n):
     def noise(I,Noise):
         n_r,n_c = Noise.shape[0],Noise.shape[1]
 
+        r_base = random.randint(0,I.shape[0]-n_r)
+        c_base = random.randint(0,I.shape[1]-n_c)
         for i in range(n_r):
             for j in range(n_c):
                 if(Noise[i, j,3]==0):
                     continue
                 for k in range(3):
-                    I[i+100,j+100,k] = Noise[i,j,k]
+                    I[i+r_base,j+c_base,k] = Noise[i,j,k]
 
         return I
     path = "processing_image/"+image+".png"
