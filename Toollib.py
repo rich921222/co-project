ba9c99246c0776ec31fe248e9f9d24d29389d8f9
wang-scott <scotts50110@gmail.com>
2025-02-03 23:19:18 +0800

    輸出變化量圖表，並更改圖標，將計算變化量的程式新增至toolib

--- Toollib.py
+++ Toollib.py
@@ -307,3 +307,24 @@ def embeding(image,n):
     io.imshow(e)
     io.show()
     io.imsave('embeding_noise/'+image+'.png',e) 
+    
+def RB_histogram_Variation_Frequency(delta_RB,image):
+    delta_RB_ravel = delta_RB.ravel()
+    delta_RB_ravel = np.abs(delta_RB_ravel)
+    bins = range(0, 16)
+    counts, edges = np.histogram(delta_RB_ravel, bins=bins)
+    print(counts)
+
+    # 繪製長條圖
+    plt.bar(edges[:-1], counts, width=1, edgecolor="black", align="edge")
+
+    # 設置橫軸範圍和標籤
+    plt.xticks(range(0, 16))
+    plt.xlabel("Variation")
+    plt.ylabel("Frequency")
+    plt.title("Grayscale Invariance approximation")
+    
+    plt.savefig(f"Variation-Frequency/{image}_APPM.png")
+    # 顯示圖表
+    plt.show()
+    
